d8e3845eac2a2add9ff63c0e2a909aef729bb3a7
feof1l <ivan.vasilov.03@mail.ru>
2024-02-24 02:25:00 +0300

    refactoring of structure

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,29 +2,9 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"strconv"
 )
-
-func main() {
-	// Используется функция http.NewServeMux() для инициализации нового рoутера, затем
-	// функцию "home" регистрируется как обработчик для URL-шаблона "/".
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/link", showLink)
-	mux.HandleFunc("/link/create", createLink)
-
-	// Используется функция http.ListenAndServe() для запуска нового веб-сервера.
-	// Мы передаем два параметра: TCP-адрес сети для прослушивания (в данном случае это "localhost:8080")
-	// и созданный рутер. Если вызов http.ListenAndServe() возвращает ошибку
-	// мы используем функцию log.Fatal() для логирования ошибок. Обратите внимание
-	// что любая ошибка, возвращаемая от http.ListenAndServe(), всегда non-nil.
-	log.Println("Запуск веб-сервера на http://127.0.0.1:8080")
-	err := http.ListenAndServe(":8080", mux)
-	log.Fatal(err)
-
-}
 
 // Создается функция-обработчик "home", которая записывает байтовый слайс, содержащий
 // текст "Привет из  LinkBox" как тело ответа.
@@ -50,7 +30,7 @@ func showLink(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	fmt.Fprintf(w, "Отображение выбранной заметки с ID %d...", id)
-	w.Write([]byte("Отображение заметки ..."))
+	//w.Write([]byte("Отображение заметки ..."))
 }
 
 // Обработчик для создание новой заметки
