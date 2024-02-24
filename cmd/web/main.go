24f91cb459e3725198d66458a3d39480e981e2bc
feof1l <ivan.vasilov.03@mail.ru>
2024-02-24 23:01:13 +0300

    edit frontend

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -6,17 +6,20 @@ import (
 )
 
 func main() {
-	// Используется функция http.NewServeMux() для инициализации нового рoутера, затем
-	// функцию "home" регистрируется как обработчик для URL-шаблона "/".
+	// инициализация нового рoутера,функцию "home" регистрируется как обработчик для URL-шаблона "/".
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/link", showLink)
 	mux.HandleFunc("/link/create", createLink)
 
-	// Используется функция http.ListenAndServe() для запуска нового веб-сервера.
-	// Мы передаем два параметра: TCP-адрес сети для прослушивания (в данном случае это "localhost:8080")
-	// и созданный рутер. Если вызов http.ListenAndServe() возвращает ошибку
-	// мы используем функцию log.Fatal() для логирования ошибок. Обратите внимание
+	//  обработчик HTTP-запросов к статическим файлам из папки "./ui/static"
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+
+	// обработчика для всех запросов, которые начинаются с "/static/". Мы убираем
+	// префикс "/static" перед тем как запрос достигнет http.FileServer
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	// запуска нового веб-сервера.
+	// параметры: TCP-адрес сети для прослушивания (в данном случае это "localhost:8080")
 	// что любая ошибка, возвращаемая от http.ListenAndServe(), всегда non-nil.
 	log.Println("Запуск веб-сервера на http://127.0.0.1:8080")
 	err := http.ListenAndServe(":8080", mux)
