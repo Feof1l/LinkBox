b77b970746ac5a6c0eb6e03de9a6c097aa358d1e
feof1l <ivan.vasilov.03@mail.ru>
2024-02-24 23:19:33 +0300

    custom filesystem

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,21 +3,24 @@ package main
 import (
 	"log"
 	"net/http"
+	"path/filepath"
 )
 
 func main() {
 	// инициализация нового рoутера,функцию "home" регистрируется как обработчик для URL-шаблона "/".
 	mux := http.NewServeMux()
+
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/link", showLink)
 	mux.HandleFunc("/link/create", createLink)
 
 	//  обработчик HTTP-запросов к статическим файлам из папки "./ui/static"
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-
+	fileServer := http.FileServer(neuteredFileSystem{http.Dir("./ui/static/")})
 	// обработчика для всех запросов, которые начинаются с "/static/". Мы убираем
 	// префикс "/static" перед тем как запрос достигнет http.FileServer
+	mux.Handle("/static", http.NotFoundHandler())
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	// запуска нового веб-сервера.
 	// параметры: TCP-адрес сети для прослушивания (в данном случае это "localhost:8080")
 	// что любая ошибка, возвращаемая от http.ListenAndServe(), всегда non-nil.
@@ -25,4 +28,30 @@ func main() {
 	err := http.ListenAndServe(":8080", mux)
 	log.Fatal(err)
 
+}
+
+type neuteredFileSystem struct {
+	fs http.FileSystem
+}
+
+func (nfs neuteredFileSystem) Open(path string) (http.File, error) {
+	f, err := nfs.fs.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	s, err := f.Stat()
+	if s.IsDir() {
+		index := filepath.Join(path, "index.html")
+		if _, err := nfs.fs.Open(index); err != nil {
+			closeErr := f.Close()
+			if closeErr != nil {
+				return nil, closeErr
+			}
+
+			return nil, err
+		}
+	}
+
+	return f, nil
 }
